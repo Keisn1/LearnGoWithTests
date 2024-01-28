30c1ba556a1db01a0ade2f2f52138dde51b536be
Kay Freyer <kay.freyer@icloud.com>
2024-01-28 08:40:10 +0100

    28/01

---
+++ blogrenderer/renderer_test.go
@@ -0,0 +1,27 @@
+package blogrenderer_test
+
+import (
+	"GoWithTests/blogposts"
+	"GoWithTests/blogrenderer"
+	"bytes"
+	approvals "github.com/approvals/go-approval-tests"
+	"testing"
+)
+
+func TestConvertHtml(t *testing.T) {
+	var (
+		aPost = blogposts.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := &bytes.Buffer{}
+		if err := blogrenderer.Render(buf, aPost); err != nil {
+			t.Fatal(err)
+		}
+		approvals.VerifyString(t, buf.String())
+	})
+}
